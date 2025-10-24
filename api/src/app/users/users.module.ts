1a2e6b818a21e029a20542fe73b151c336c6350b
Madison Walker <madwalker231@gmail.com>
2025-10-24 14:02:34 -0500

    Ran user test. created a user to ensure that API communication is working and JWT works properly.

--- api/src/app/users/users.module.ts
+++ api/src/app/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../database/entities/user.entity';
+import { Organization } from '../database/entities/organization.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Organization])],
   providers: [UserService],
   exports: [UserService],
 })
--- api/src/app/users/users.service.ts
+++ api/src/app/users/users.service.ts
@@ -1,14 +1,42 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "../database/entities/user.entity";
+import { Organization } from "../database/entities/organization.entity";
 import { Repository } from "typeorm";
+import { UserRole } from "@turbovet-tasks/data-models";
 
 @Injectable()
-export class UserService {
+export class UserService implements OnModuleInit {
+  private readonly logger = new Logger (UserService.name)
+
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @InjectRepository(Organization)
+    private orgRepository: Repository<Organization>,
   ) {}
+
+  async onModuleInit() {
+    const adminUser = await this.usersRepository.findOne({
+      where: { email: 'admin@test.com' },
+    });
+    if (!adminUser) {
+      this.logger.log('No admin user found. Creating test data...');
+      const org = this.orgRepository.create({ name: 'Test Org' });
+      await this.orgRepository.save(org);
+      this.logger.log(`Created organization: ${org.name}`);
+
+      const user = this.usersRepository.create({
+        email: 'admin@test.com',
+        password: 'password123',
+        role: UserRole.ADMIN,
+        organization: org,
+      });
+      await this.usersRepository.save(user);
+      this.logger.log(`Created admin user: ${user.email}`);
+    }
+  }
+
 
   /**
    * Finds User by email, and explicitly includes the password.
