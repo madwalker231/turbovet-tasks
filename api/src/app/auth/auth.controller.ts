4a467c1451492e1da92392957da5e0ba80d478f8
Madison Walker <madwalker231@gmail.com>
2025-10-24 13:13:05 -0500

    Implemented public decorator for JWT access.

--- api/src/app/auth/auth.controller.ts
+++ api/src/app/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
